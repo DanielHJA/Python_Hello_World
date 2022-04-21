aeae87719e97068eea042ab3e0ddf6bfaf81a8d0
Daniel Hjärtström <danielhjartstrom@Daniels-MacBook-Pro.local>
2022-04-21 10:23:59 +0200

    New feature

--- helloworld/main.py
+++ helloworld/main.py
@@ -8,6 +8,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hello world")  # Press ⌘F8 to toggle the breakpoint.
+    print("Goodbye world")  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
